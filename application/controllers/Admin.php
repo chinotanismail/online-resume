f5f4cac992810b1a430ad2b2b974fa4a29becdfb
Chino Ismail <chino.ismail@gmail.com>
2020-12-27 15:31:29 +0700

    Removed personal email

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -104,7 +104,7 @@ class Admin extends CI_Controller {
     $mail->isSMTP();
     $mail->Host     = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'chino.ismail@gmail.com';
+    $mail->Username = '';
     $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port     = 587;
