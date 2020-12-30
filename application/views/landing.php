29af1e0961657288a58df04abc531c59af0c1ad1
Chino Ismail <chino.ismail@gmail.com>
2020-12-30 11:32:32 +0700

    added new pics and fixed meta data for social media previews

--- application/views/landing.php
+++ application/views/landing.php
@@ -6,6 +6,11 @@
     <meta name="description" content="An overview of Chino's background and recent work.">
     <meta name="keywords" content="<?php foreach($words as $word){echo $word->word.", ";}?>">
     <meta name="author" content="Chino Tan Ismail">
+    <meta property="og:title" content="Chino Tan Ismail">
+    <meta property="og:description" content="An overview of Chino's background and recent work.">
+    <meta property="og:image" content="<?php echo asset_url();?>img/preview.png">
+    <meta property="og:url" content="http://chinotanismail.com">
+
     <title>Chino Tan Ismail</title>
     <link rel="icon" href="<?php echo asset_url();?>img/favicon.png" sizes="16x16" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
