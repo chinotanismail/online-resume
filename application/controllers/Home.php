82f4caabb123fc4bf12e7baf94881d03aafbefc8
Chino Ismail <chino.ismail@gmail.com>
2020-12-31 11:48:20 +0700

    Added email notification on new message received

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -39,6 +39,23 @@ class Home extends CI_Controller {
 
 	public function send_message()
 	{
+		$this->load->library('phpmailer_lib');
+    $mail = $this->phpmailer_lib->load();
+
+    $mail->isSMTP();
+    $mail->Host     = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = '';
+    $mail->Password = '';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port     = 587;
+    $mail->setFrom('chino.ismail@gmail.com', 'Chino Ismail');
+    $mail->addReplyTo($_POST['email_address'], $_POST['name']);
+    $mail->addAddress('chino.ismail@gmail.com');
+    $mail->Subject = 'New Message from '.$_POST['name'];
+    $mail->isHTML(true);
+    $mail->Body = $_POST['message'];
+
 		echo $this->message_model->save($_POST);
 	}
 
